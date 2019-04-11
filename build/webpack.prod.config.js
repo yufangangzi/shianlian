4ea1ea3412b1e0138e7b2078c694e73810f7bd51
陈刚 <769063757@qq.com>
2019-04-11 15:57:50 +0800

    添加处理es6

--- build/webpack.prod.config.js
+++ build/webpack.prod.config.js
@@ -79,4 +79,14 @@ module.exports = merge(webpackDevConfig, {
         }
     }
   },
+  performance: {
+    hints: "warning", // 枚举
+    maxAssetSize: 30000000, // 整数类型（以字节为单位）
+    maxEntrypointSize: 50000000, // 整数类型（以字节为单位）
+    assetFilter: function(assetFilename) {
+    // 提供资源文件名的断言函数
+    return assetFilename.endsWith('.css') || assetFilename.endsWith('.js');
+    
+    }
+},
 })
