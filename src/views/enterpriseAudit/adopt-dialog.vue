faccdec0039eb0f72564a52fdf828d882921303c
熊翠 <xiongcui@sinochem.com>
2019-04-24 18:28:08 +0800

    ui更新

--- src/views/enterpriseAudit/adopt-dialog.vue
+++ src/views/enterpriseAudit/adopt-dialog.vue
@@ -1,13 +1,16 @@
 <template>
-  <el-dialog title="提示" :visible.sync="adoptVisible" width="500px" :before-close="handleClose">
-    <h3 class="adoptTitle">通过审核！</h3>
-    <p class="adoptCt">
-      您已经通过该企业的加入食安物链申请，该企业现已上链。
-    </p>
-    <span slot="footer" class="dialog-footer">
-      <!-- <el-button @click="adoptCancel">取 消</el-button> -->
-      <el-button type="primary" @click="adoptOk">确 认</el-button>
-    </span>
+  <el-dialog title="提示" :visible.sync="adoptVisible" width="400px" :before-close="handleClose">
+    <div class="adoptBox">
+      <i class="el-icon-circle-check"></i>
+      <h3 class="adoptTitle">通过审核</h3>
+      <p class="adoptCt">
+        您已经通过该企业的加入食安物链申请，该企业现已上链。
+      </p>
+      <span slot="footer" class="dialog-footer">
+        <!-- <el-button @click="adoptCancel">取 消</el-button> -->
+        <el-button type="primary" @click="adoptOk">确 认</el-button>
+      </span>
+    </div>
   </el-dialog>
 </template>
 <script>
@@ -15,7 +18,7 @@
     props:{
       adoptVisible: {
         type: Boolean,
-        default: false
+        default: true
       },
       adoptOk: {
         type: Function
@@ -34,10 +37,25 @@
 </script>
 <style scoped>
 .adoptTitle{
-  text-align: center;
-  margin-bottom: 20px;
+  margin-top: 14px;
+  margin-bottom: 8px;
+  font-size: 16px;
+  color: #514C4A;
+  font-weight: normal;
+}
+.adoptBox{
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+.adoptBox .el-icon-circle-check{
+  font-size: 40px;
+  color: #19C162;
 }
 .adoptCt{
-  text-align: center;
+  font-size: 12px;
+  color: #898989;
+  margin-bottom: 35px;
 }
 </style>
--- src/views/enterpriseAudit/details.vue
+++ src/views/enterpriseAudit/details.vue
@@ -2,54 +2,53 @@
   <div class="details">
     <div class="gailan">概览/企业审核</div>
     <div class="detailBox">
-      <div class="top-bar">企业审核</div>
+      <div class="top-bar">企业信息</div>
       <div class="detailCt">
-        <h3 class="detailTitle">企业信息</h3>
         <p>
-          <span class="detailLeft">企业名称</span>
+          <span class="detailLeft">企业名称：</span>
           <span class="detailRt">青岛大成集团</span>
         </p>
         <p>
-          <span class="detailLeft">统一社会信用代码</span>
+          <span class="detailLeft">统一社会信用代码：</span>
           <span class="detailRt">9144030071526726XG</span>
         </p>
         <p>
-          <span class="detailLeft">注册地址</span>
+          <span class="detailLeft">注册地址：</span>
           <span class="detailRt">山东省青岛市黄岛区铁橛山路1111号石桥大厦</span>
         </p>
         <p>
-          <span class="detailLeft">联络地址</span>
+          <span class="detailLeft">联络地址：</span>
           <span class="detailRt">山东省青岛市黄岛区铁橛山路1111号石桥大厦</span>
         </p>
         <p>
-          <span class="detailLeft">法人代表</span>
+          <span class="detailLeft">法人代表：</span>
           <span class="detailRt">赵敏</span>
         </p>
         <p>
-          <span class="detailLeft">归属地区</span>
+          <span class="detailLeft">归属地区：</span>
           <span class="detailRt">山东省青岛市黄岛区</span>
         </p>
         <p>
-          <span class="detailLeft">生产许可证</span>
+          <span class="detailLeft">生产许可证：</span>
           <span class="detailRt">12356450</span>
         </p>
         <p>
-          <span class="detailLeft">种养殖许可证</span>
+          <span class="detailLeft">种养殖许可证：</span>
           <span class="detailRt">12356450</span>
         </p>
         <p>
-          <span class="detailLeft">上链类型</span>
+          <span class="detailLeft">上链类型：</span>
           <span class="detailRt">产地链/销售链</span>
         </p>
         <p>
-          <h3>工商营业执照</h3>
-          <img src="../../assets/img/loginbg.png"/>
+          <span class="detailLeft">工商营业执照：</span>
+          <span class="detailRt"><img src="../../assets/img/loginbg.png"/></span>
         </p>
       </div>
       <div class="detailBt">
-        <el-button @click="$router.replace('/enterpriseAudit')">返回</el-button>
-        <el-button type="primary" @click="adopt">通过</el-button>
-        <el-button type="info" @click="refuse">拒绝</el-button>
+        <el-button @click="$router.replace('/enterpriseAudit')" class="backBtn">返回</el-button>
+        <el-button type="primary" @click="adopt" class="adoptBtn">通过</el-button>
+        <el-button type="info" @click="refuse" class="refuseBtn">拒绝</el-button>
       </div>
     </div>
     <adopt-dialog :adoptVisible="adoptVisible" :adoptOk="adoptOk"></adopt-dialog>
@@ -126,25 +125,47 @@ import RefuseDialog from './refuse-dialog.vue'
   background: rgba(0, 135, 237, 1);
 }
 .detailCt{
-  padding: 40px 60px;
+  padding: 20px 165px;
+  color: #555555;
 }
 .detailCt p{
-  margin-top: 20px;
-  margin-bottom: 20px;
+  font-size: 18px;
+  margin-bottom: 30px;
 }
 .detailCt p .detailLeft{
-  width: 50px;
+  width: 162px;
   display: inline-block;
   margin-right: 20px;
+  vertical-align: top;
 }
-.detailCt p h3{
-  font-weight: normal;
+.detailCt p .detailRt{
+  width: 75%;
+  display: inline-block;
+  font-size: 16px;
+  font-weight: bold;
 }
 .detailCt img{
-  width: 90%;
-  margin-top: 20px;
+  width: 100%;
 }
 .detailBt{
-  text-align: center;
+  display: flex;
+  justify-content: center;
+}
+.backBtn{
+  width: 110px;
+  border: 1px solid #8C8C8C;
+  margin-left: 100px;
+  margin-right: 60px;
+}
+.adoptBtn{
+  width: 110px;
+  background: #4DD287;
+  border: 1px solid #44B073;
+  margin-right: 60px;
+}
+.refuseBtn{
+  width: 110px;
+  background: #FF5454;
+  border: 1px solid #CE1D1D;
 }
 </style>
